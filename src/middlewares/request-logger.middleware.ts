443f40c290d9379e4dfe5ec47106dda4e77c637e
Sanjay <sanjaykeloth321@gmail.com>
2024-08-31 18:57:37 +0530

    chore(logging): change request logger level to http

--- src/middlewares/request-logger.middleware.ts
+++ src/middlewares/request-logger.middleware.ts
@@ -2,7 +2,7 @@ import morgan from "morgan";
 import { logger } from "../lib";
 
 const stream = {
-  write: (message: string) => logger.info(message.trim()),
+  write: (message: string) => logger.http(message.trim()),
 };
 
 export const requestLogger = morgan("dev", { stream });
